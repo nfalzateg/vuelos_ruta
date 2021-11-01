ffb2a88fa35fe558771f293258d0309bcb5b54d6
nfalzateg <nelson-1998-04@hotmail.com>
2021-11-01 09:18:33 -0500

    Subimos el query de datos

---
+++ inserccion_datos.sql
@@ -0,0 +1,18 @@
+USE bebold;
+
+INSERT INTO origen (idOrigen, nAeropuerto, ciudad) VALUES (1, "Aeropuerto el eden", "Armenia");
+INSERT INTO origen (idOrigen, nAeropuerto, ciudad) VALUES (2, "Aeropuerto matecaña", "Pereira");
+INSERT INTO origen (idOrigen, nAeropuerto, ciudad) VALUES (3, "Aeropuerto el dorado", "Bogota");
+INSERT INTO origen (idOrigen, nAeropuerto, ciudad) VALUES (4, "Aeropuerto simon bolivar", "Santamarta");
+INSERT INTO origen (idOrigen, nAeropuerto, ciudad) VALUES (5, "Aeropuerto rafael nuñez", "Cartagena");
+
+SELECT * FROM origen;
+
+INSERT INTO destino (idDestino, nAeropuerto, ciudad) VALUES (1, "Aeropuerto el eden", "Armenia");
+INSERT INTO destino (idDestino, nAeropuerto, ciudad) VALUES (2, "Aeropuerto matecaña", "Pereira");
+INSERT INTO destino (idDestino, nAeropuerto, ciudad) VALUES (3, "Aeropuerto el dorado", "Bogota");
+INSERT INTO destino (idDestino, nAeropuerto, ciudad) VALUES (4, "Aeropuerto simon bolivar", "Santamarta");
+INSERT INTO destino (idDestino, nAeropuerto, ciudad) VALUES (5, "Aeropuerto rafael nuñez", "Cartagena");
+
+SELECT * FROM destino;
+
